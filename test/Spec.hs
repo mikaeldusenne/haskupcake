58102aefc6a1d56757404ff7c13611160eef7866
Mikaunix <mikaunix@gmail.com>
2018-03-09 12:26:51 -0500

    TestResults.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,13 +6,13 @@ import qualified Data.ByteString.Char8 as BS
 
 import Lib
 import Config
-
+import TestResults
 
 main :: IO ()
 main = do
-  tok <- BS.readFile "token"
+  config <- readConfig "config.json"
   hspec $ do
     describe "Lib.listServices" $ do
       it "lists the available services for a pic-sure thing" $ do
-        listServices (Config {domain = "https://pmsdn-dev.hms.harvard.edu", token = tok}) >>= (`shouldBe` ["PMSDN-dev"])
+        listServices config >>= (`shouldBe` resultListService)
   
